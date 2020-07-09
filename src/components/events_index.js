0ed4ab699529b08a9c6688c6fedb94dac77484f8
k-kzy <rip.to.the.cat@gmail.com>
2020-07-09 15:08:20 +0900

    feat：impleme event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -13,7 +13,7 @@ class EventsIndex extends React.Component {
 
   renderEvents() {
     return _.map(this.props.events, event => (
-      <tr key="{event.id}">
+      <tr key={event.id}>
         <td>{event.id}</td>
         <td>{event.title}</td>
         <td>{event.body}</td>
